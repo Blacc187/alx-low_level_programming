d0e3c5ea0b6140509bd244588bd0ac69ee03ff7c
blacc187 <emomotimi.timiala@gmail.com>
2023-03-22 20:48:37 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * -isalpha - function thatchecks for alphabets
+ *
+ *  @c: parameter to be checked
+ *
+ *  Return:1 is it is n alphabet otherwise
+ *  return 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || (c >= 65 && c <= 90))
+		 return(1);
+	else
+		return(0);
+}
